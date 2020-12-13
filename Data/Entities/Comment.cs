8485faacd78e69bdf5d86efe5190ac7a073c63b9
Mario Toshev <mario.a.toshev@gmail.com>
2020-12-13 17:02:41 +0200

    Models
    some models are up

---
+++ Data/Entities/Comment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamDeliriumProject.Data.Entities
+{
+    public class Comment
+    {
+    }
+}
---
+++ Data/Entities/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamDeliriumProject.Data.Identity;
+
+namespace TeamDeliriumProject.Data.Entities
+{
+    public class Post
+    {
+        [Key]
+        public int id;
+        [Required]
+        public string title;
+
+        [Required]
+        public string description;
+
+        public User creator;
+
+        public List<Comment> comments;
+
+        //public int likes;
+
+    }
+}
